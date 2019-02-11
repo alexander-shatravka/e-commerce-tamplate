e7d3b7d94fee56c2be530375c00422cc4586583e
Alexander Shatravka <shatravgo2@gmail.com>
2019-02-11 10:18:43 +0200

    reupload project

---
+++ config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://test4machine.viral.com.ua/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://test4machine.viral.com.ua/');
+
+// DIR
+define('DIR_APPLICATION', '/home/viral/web/test4machine.viral.com.ua/public_html/catalog/');
+define('DIR_SYSTEM', '/home/viral/web/test4machine.viral.com.ua/public_html/system/');
+define('DIR_IMAGE', '/home/viral/web/test4machine.viral.com.ua/public_html/image/');
+define('DIR_STORAGE', '/home/viral/web/test4machine.viral.com.ua/public_html/storage/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/theme/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_MODIFICATION', DIR_STORAGE . 'modification/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', '116.203.17.49');
+define('DB_USERNAME', 'viral_viral');
+define('DB_PASSWORD', 'ftpMbKNa9w');
+define('DB_DATABASE', 'viral_test4m');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
